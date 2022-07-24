ff2ca36be057e9336feca194af299a982d196f1f
aditya3011007 <108451255+aditya3011007@users.noreply.github.com>
2022-07-24 23:08:21 +0530

    Update app.js

--- node_passport_login/app.js
+++ node_passport_login/app.js
@@ -6,14 +6,10 @@ const flash = require('connect-flash');
 const session = require('express-session');
 
 const app = express();
-
-// Passport Config
 require('./config/passport')(passport);
 
-// DB Config
 const db = require('./config/keys').mongoURI;
 
-// Connect to MongoDB
 mongoose
   .connect(
     db,
@@ -22,14 +18,13 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// EJS
+
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-// Express body parser
 app.use(express.urlencoded({ extended: true }));
 
-// Express session
+
 app.use(
   session({
     secret: 'secret',
@@ -38,14 +33,12 @@ app.use(
   })
 );
 
-// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Connect flash
+
 app.use(flash());
 
-// Global variables
 app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
@@ -53,7 +46,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-// Routes
+
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
